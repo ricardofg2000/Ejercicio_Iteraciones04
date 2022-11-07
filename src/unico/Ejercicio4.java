84ee511d1b54482877edf734b81ef86f97dc7ee5
ricardofg2000 <ricardofg2000@gmail.com>
2022-11-07 10:08:13 +0100

    añadimos 3 iteraciones distintas "while" "do" y "for"

--- src/unico/Ejercicio4.java
+++ src/unico/Ejercicio4.java
@@ -6,7 +6,8 @@ public class Ejercicio4 {
 
 	public static void main(String[] args) {
 		/*
-		 Escribir una aplicación que pida un número n, y escriba los números desde 1 hasta n.
+		 Escribir una aplicación que pida un número n, y 
+		 escriba los números desde 1 hasta n.
 		 */
 
 		Scanner leer = new Scanner (System.in);
@@ -16,12 +17,22 @@ public class Ejercicio4 {
 		System.out.print("Introduzca un número ");
 		n = Integer.parseInt(leer.nextLine());
 		
-		 do {
+		while (i <= n) {
 			System.out.println(i);
 			i++;
-			} while (i != n+1);
+		}
 		
+		i = 1;
+		System.out.println();
+		do {
+			System.out.println(i);
+			i++;
+			} while (i <= n);
 		
+		System.out.println();
+		for (i = 1; i<=n; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
